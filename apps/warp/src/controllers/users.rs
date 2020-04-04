94ae634fe23555c0dc9f7f15e2672be09ca440f3
Hien Pham <hien.pham2@tiki.vn>
2020-04-04 15:27:37 +0700

    feat: make warp works with di

---
+++ apps/warp/src/controllers/users.rs
@@ -0,0 +1,9 @@
+use std::sync::Arc;
+use warp::reject;
+use rwebapi_users::UserService;
+use crate::error::ApiError;
+
+pub async fn get_user( user_services: Arc<Box<&dyn UserService>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let users = user_services.as_ref().users().map_err(|e| reject::custom(ApiError::from(e)))?;
+    Ok(warp::reply::json(&users))
+}
